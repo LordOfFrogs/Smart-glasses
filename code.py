0423f00cab2a4221f66d3e513e773f70c2e05e46
LordOfFrogs <ndtech260@gmail.com>
2024-01-09 22:54:27 -0500

    Updated code from Pico

--- code.py
+++ code.py
@@ -17,20 +17,28 @@ HALL_PIN_1 = board.GP6
 HALL_PIN_2 = board.GP7
 HALL_PIN_3 = board.GP8
 HALL_PIN_4 = board.GP9
-CLICK_DELAY_MS = 100
+CLICK_DELAY_MS = 200
 ABS_MOUSE_BOUNDS = 32767
 
-hall1_hist = [True, True, True, True, True]
-hall2_hist = [True, True, True, True, True]
-hall3_hist = [True, True, True, True, True]
-hall4_hist = [True, True, True, True, True]
+hall1_hist = []
+hall2_hist = []
+hall3_hist = []
+hall4_hist = []
 
-i2c = busio.I2C(board.GP15, board.GP14)
+state = [True, True, True, True]
+
+for i in range(0, CLICK_DELAY_MS//SAMPLERATE_DELAY_MS):
+  hall1_hist.append(True)
+  hall2_hist.append(True)
+  hall3_hist.append(True)
+  hall4_hist.append(True)
+
+#i2c = busio.I2C(board.GP15, board.GP14)
 # Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 #                                   id, address
-bno = adafruit_bno055.BNO055_I2C(i2c, 0x28)
+#bno = adafruit_bno055.BNO055_I2C(i2c, 0x28)
 
-zero_yaw = 0.0
+#zero_yaw = 0.0
 
 tare_prev = False
 
@@ -55,16 +63,16 @@ hall4_pin = digitalio.DigitalInOut(HALL_PIN_4)
 hall4_pin.pull = digitalio.Pull.UP
 
 # Setup input
-#MouseTo.setScreenResolution(SCREEN_WIDTH, SCREEN_HEIGHT)
-#MouseTo.home()
+# MouseTo.setScreenResolution(SCREEN_WIDTH, SCREEN_HEIGHT)
+# MouseTo.home()
 m = Mouse(usb_hid.devices)
 
 def UpdateHistoryLists(hall1, hall2, hall3, hall4):
-  for i in range(CLICK_DELAY_MS//SAMPLERATE_DELAY_MS, 1, -1):
-    hall1_hist[i] = hall1_hist[i-1]
-    hall2_hist[i] = hall2_hist[i-1]
-    hall3_hist[i] = hall3_hist[i-1]
-    hall4_hist[i] = hall4_hist[i-1]
+  for i in range(1, CLICK_DELAY_MS//SAMPLERATE_DELAY_MS):
+    hall1_hist[-i] = hall1_hist[-i-1]
+    hall2_hist[-i] = hall2_hist[-i-1]
+    hall3_hist[-i] = hall3_hist[-i-1]
+    hall4_hist[-i] = hall4_hist[-i-1]
 
   hall1_hist[0] = hall1
   hall2_hist[0] = hall2
@@ -72,7 +80,7 @@ def UpdateHistoryLists(hall1, hall2, hall3, hall4):
   hall4_hist[0] = hall4
 
 def ClearHistoryLists():
-  for i in range(CLICK_DELAY_MS//SAMPLERATE_DELAY_MS, 1, -1):
+  for i in range(CLICK_DELAY_MS//SAMPLERATE_DELAY_MS-1, 1, -1):
     hall1_hist[i] = hall1_hist[0]
     hall2_hist[i] = hall2_hist[0]
     hall3_hist[i] = hall3_hist[0]
@@ -97,7 +105,7 @@ while True:
   hall2 = hall2_pin.value
   hall3 = hall3_pin.value
   hall4 = hall4_pin.value
-  
+  #print(f'{hall1}, {hall2}, {hall3}, {hall4}')
   # Update history list
   UpdateHistoryLists(hall1, hall2, hall3, hall4)
 
@@ -105,70 +113,87 @@ while True:
   hall2_prev = hall2_hist[-1]
   hall3_prev = hall3_hist[-1]
   hall4_prev = hall4_hist[-1]
+  
+  # hall1 = hall1_hist[1]
+  # hall2 = hall2_hist[1]
+  # hall3 = hall3_hist[1]
+  # hall4 = hall4_hist[1]
+  
+  print(f'{hall1}, {hall2}, {hall3}, {hall4},\t {hall1_prev}, {hall2_prev}, {hall3_prev}, {hall4_prev}')
 
   # Clicking logic
   if (
-    (hall1 and hall2 and hall3 and hall4) and
-    (not hall1_prev and hall2_prev and hall3_prev and hall4_prev)
+    (hall1_prev and hall2_prev and hall3_prev and hall4_prev) and
+    (not state[0] and state[1] and state[2] and state[3]) and
+    not (hall1 != hall1_prev or hall2 != hall2_prev or hall3 != hall3_prev or hall4 != hall4_prev)
   ):
     m.press(Mouse.LEFT_BUTTON)
+    state = [True, True, True, True]
     ClearHistoryLists()
   elif (
-    (not hall1 and hall2 and hall3 and hall4) and
-    (hall1_prev and hall2_prev and hall3_prev and hall4_prev)
+    (not hall1_prev and hall2_prev and hall3_prev and hall4_prev) and
+    (state[0] and state[1] and state[2] and state[3]) and
+    not (hall1 != hall1_prev or hall2 != hall2_prev or hall3 != hall3_prev or hall4 != hall4_prev)
   ):
     m.release(Mouse.LEFT_BUTTON)
+    state = [False, True, True, True]
     ClearHistoryLists()
   elif (
-    (hall1 and hall2 and hall3 and hall4) and
-    (not hall1_prev and not hall2_prev and hall3_prev and hall4_prev)
+    (hall1_prev and hall2_prev and hall3_prev and hall4_prev) and
+    (not state[0] and not state[1] and state[2] and state[3]) and
+    not (hall1 != hall1_prev or hall2 != hall2_prev or hall3 != hall3_prev or hall4 != hall4_prev)
   ):
     m.press(Mouse.RIGHT_BUTTON)
+    state = [True, True, True, True]
     ClearHistoryLists()
   elif (
-    (not hall1 and not hall2 and hall3 and hall4) and
-    (hall1_prev and hall2_prev and hall3_prev and hall4_prev)
+    (not hall1_prev and not hall2_prev and hall3_prev and hall4_prev) and
+    (state[0] and state[1] and state[2] and state[3]) and
+    not (hall1 != hall1_prev or hall2 != hall2_prev or hall3 != hall3_prev or hall4 != hall4_prev)
   ):
     m.release(Mouse.RIGHT_BUTTON)
+    state = [False, False, True, True]
     ClearHistoryLists()
   elif (
     (hall1 and hall2 and hall3 and hall4) and
     (not hall1_prev and not hall2_prev and not hall3_prev and hall4_prev)
   ):
     m.press(Mouse.MIDDLE_BUTTON)
+    state = [True, True, True, True]
     ClearHistoryLists()
   elif (
     (not hall1 and not hall2 and not hall3 and hall4) and
     (hall1_prev and hall2_prev and hall3_prev and hall4_prev)
   ):
     m.release(Mouse.MIDDLE_BUTTON)
+    state = [False, False, False, True]
     ClearHistoryLists()
   
 
-  # BNO055
-  yaw = bno.euler[0] - zero_yaw
-  pitch = bno.euler[2]
-  print(yaw, pitch)
-  yaw = NormalizeAngle(yaw)
-  pitch = -NormalizeAngle(180 - pitch) # Use flipped pitch because upside-down
+#   # BNO055
+#   yaw = bno.euler[0] - zero_yaw
+#   pitch = bno.euler[2]
+#   print(yaw, pitch)
+#   yaw = NormalizeAngle(yaw)
+#   pitch = -NormalizeAngle(180 - pitch) # Use flipped pitch because upside-down
 
-#   tare_btn = not digitalRead(TARE_PIN)
-#   if (tare_btn and not tare_prev):
-#     # Tare
-#     zero_yaw = fmod(yaw + zero_yaw + 360, 360)
-#     #MouseTo.home()
+# #   tare_btn = not digitalRead(TARE_PIN)
+# #   if (tare_btn and not tare_prev):
+# #     # Tare
+# #     zero_yaw = fmod(yaw + zero_yaw + 360, 360)
+# #     #MouseTo.home()
   
-#   tare_prev = tare_btn
+# #   tare_prev = tare_btn
 
-  yaw = max(-X_BOUND, min(yaw, X_BOUND))
-  xPos = (yaw+X_BOUND) * ABS_MOUSE_BOUNDS // (2.0*X_BOUND)
+#   yaw = max(-X_BOUND, min(yaw, X_BOUND))
+#   xPos = (yaw+X_BOUND) * ABS_MOUSE_BOUNDS // (2.0*X_BOUND)
   
-  pitch = max(-Y_BOUND, min(pitch, Y_BOUND))
-  yPos = (pitch+Y_BOUND) * ABS_MOUSE_BOUNDS // (2.0*Y_BOUND)
+#   pitch = max(-Y_BOUND, min(pitch, Y_BOUND))
+#   yPos = (pitch+Y_BOUND) * ABS_MOUSE_BOUNDS // (2.0*Y_BOUND)
 
-  #MouseTo.setTarget(xPos, yPos, false)
-  #MouseTo.move()
-  m.move(x=int(xPos), y=int(yPos))
+#   #MouseTo.setTarget(xPos, yPos, false)
+#   #MouseTo.move()
+#   m.move(x=int(xPos), y=int(yPos))
   
   time.sleep(SAMPLERATE_DELAY_MS/1000)
 
